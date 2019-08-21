2ef3ad5c387550aaaa5376dfc7b66464c67c0a36
divineanii <40550530+aniisunny@users.noreply.github.com>
2019-08-22 00:22:04 +0530

    New version

--- app/src/main/java/com/example/rgbcoloring/MainActivity.java
+++ app/src/main/java/com/example/rgbcoloring/MainActivity.java
@@ -1,15 +1,10 @@
 package com.example.rgbcoloring;
 
 import android.graphics.Color;
-import android.graphics.drawable.ColorDrawable;
-import android.graphics.drawable.Drawable;
 import android.graphics.drawable.GradientDrawable;
-import android.graphics.drawable.ShapeDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
-import android.widget.ImageView;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
